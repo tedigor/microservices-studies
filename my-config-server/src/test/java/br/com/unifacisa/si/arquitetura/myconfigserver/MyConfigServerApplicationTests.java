cd15bb3722be5ef6b67f28eecfcfb1b03c20d9fe
Ted Medeiros <tedigormedeiros@gmail.com>
2020-09-25 09:46:16 -0300

    Subindo projeto de estudo

---
+++ my-config-server/src/test/java/br/com/unifacisa/si/arquitetura/myconfigserver/MyConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.unifacisa.si.arquitetura.myconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
