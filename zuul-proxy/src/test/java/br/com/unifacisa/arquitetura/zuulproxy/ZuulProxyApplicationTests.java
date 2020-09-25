ba87144b7ea4b2d93b093b446dfd09c7d16523cc
Ted Medeiros <tedigormedeiros@gmail.com>
2020-09-25 17:17:58 -0300

    Subindo zuul-proxy

---
+++ zuul-proxy/src/test/java/br/com/unifacisa/arquitetura/zuulproxy/ZuulProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.unifacisa.arquitetura.zuulproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
